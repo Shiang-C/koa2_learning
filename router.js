846667e2b970aafeff8bdb9146d93281665c8cc7
shiang <shiang20160314@gmail.com>
2017-06-22 17:57:53 +0800

    測試用

--- router.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router')
-const router = new Router()
-const user = require('./controller/user')
-
-router.post('/user/login', user.login)
-router.get('/user/profile', user.profile)
-
-module.exports = router  //對外可以訪問到此文件
